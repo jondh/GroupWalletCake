6f5ad60d2d521ddaa5971eae039839f2f05fe7a0
jondh <jondh@ymail.com>
2013-12-11 16:02:27 -0600

    start peer prog

--- app/Controller/TransactionsController.php
+++ app/Controller/TransactionsController.php
@@ -4,6 +4,17 @@
 		public function index(){
 		
 		}	
+		
+		public function add($wallet_id, $other_id){
+			if($this->request->is('post')){
+				$this->Transaction->create();
+				 if ($this->Transaction->save($this->request->data)) {
+               		 $this->Session->setFlash(__('The user has been saved'));
+        			 return $this->redirect(array('controller' => 'WalletRelations', 'action' => 'index', $wallet_id);
+           		 }
+           		 $this->Session->setFlash(__('The user could not be saved. Please, try again'));
+			}
+		}
 	
 	}
 ?>
